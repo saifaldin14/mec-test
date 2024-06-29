e30e67fe44e937a110dd785943b06dbf59aabf65
saifaldin14 <saifaldindeveloper@gmail.com>
2024-06-29 17:34:03 -0400

    Create executor.js

---
+++ packages/node/executor.js
@@ -0,0 +1,66 @@
+import { Worker } from "node:worker_threads";
+import { MecStatus } from "../util/env.js";
+import makeDebug from "debug";
+import util from "util";
+
+// Debug logger
+const debug = makeDebug("NodeExecutor");
+
+/**
+ * Node.js test executor using worker threads
+ */
+export default class NodeExecutor {
+  /**
+   * Create executor instance.
+   *
+   * @param {string[]} targets - Test targets.
+   */
+  constructor(targets) {
+    this.targets = targets;
+    debug("NodeExecutor targets:", targets);
+  }
+
+  /**
+   * Execute tests.
+   *
+   * @returns {Promise<Object>} Test result status.
+   */
+  async execute() {
+    const testSuites = this.targets.length;
+    let testFailed = false;
+    console.log("Testing in Node.js", process.version);
+
+    return new Promise(resolve => {
+      const workers = [];
+
+      this.targets.forEach(target => {
+        // Create a new worker
+        const worker = new Worker("./" + target);
+
+        // Listen for messages and wrap console
+        worker.on("message", message => {
+          if (message === MecStatus.Fail)
+            testFailed = true;
+
+          // If a status is recovered, update the number of test suites
+          if (message === MecStatus.Pass || message === MecStatus.Fail)
+            testSuites--;
+        });
+
+        workers.push(worker);
+      });
+
+      // Wait for all workers to finish
+      Promise.all(
+        workers.map(w => new Promise(res => w.once("exit", res)))
+      ).then(() => {
+        if (testSuites === 0) {
+          const status = MecStatus.Fail ? testFailed : MecStatus.Pass;
+          return resolve({
+            status
+          });
+        }
+      });
+    });
+  }
+}
