9b825a6cd0e8016b032c6351cb6ddd925c3094ff
saifaldin14 <saifaldindeveloper@gmail.com>
2024-06-29 17:20:16 -0400

    Create lib.js

---
+++ packages/mock/lib.js
@@ -0,0 +1,9 @@
+const sinon = await import("sinon");
+
+// Sinon interfaces
+export const stub = sinon.stub;
+export const spy = sinon.spy;
+export const mock = sinon.mock;
+export const replace = sinon.replace;
+export const fake = sinon.fake;
+export const restore = sinon.fake;
