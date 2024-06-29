3a836ec82456a053046eb31cab86f3f4d8537054
saifaldin14 <saifaldindeveloper@gmail.com>
2024-06-29 17:39:09 -0400

    Create emit.js

---
+++ packages/browser/emit.js
@@ -0,0 +1,39 @@
+/**
+ * Event emitter class.
+ */
+export default class EventEmitter {
+  /**
+   * Create new EventEmitter instance.
+   */
+  constructor() {
+    /**
+     * Map of event names to arrays of callback functions.
+     * @private
+     */
+    this.callbacks = {};
+  }
+
+  /**
+   * Subscribe to an event.
+   *
+   * @param {string} event - Name of the event.
+   * @param {Function} cb - Callback function.
+   */
+  on(event, cb) {
+    if (!this.callbacks[event]) this.callbacks[event] = [];
+    this.callbacks[event].push(cb);
+  }
+
+  /**
+   * Emit an event. Calls all subscribed callbacks.
+   *
+   * @param {string} event - Name of the event.
+   * @param {...*} [args] - Arguments to pass to callbacks.
+   */
+  emit(event, ...args) {
+    const cbs = this.callbacks[event];
+    if (cbs) {
+      cbs.forEach(cb => cb(...args));
+    }
+  }
+}
